194de812aff270bfdf33a869446e1d7b118c5b3f
Yusuf Ahmed <yusufahmed172@gmail.com>
2020-12-20 19:54:09 -0500

    fixed repo :( ♥️ v1.0

--- app/api/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
- Wsimple v1.0
- Copyright (c) 2020 Chromazmoves
-"""
-from .api import Wsimple
-from .errors import LoginError, MethodInputError
-from .errors import InvalidAccessTokenError, InvalidRefreshTokenError
-from .errors import WSOTPUser, WSOTPError, WSOTPLoginError
--- app/static/js/build.js
+++
@@ -1,1 +0,0 @@
-export const build = "01/10/2020";
